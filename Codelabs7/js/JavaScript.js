68df7932d1f738a5e2c39620277014c12acb26fa
LAPTOPDELL\huydu <huyducphamofficial@gmail.com>
2020-08-17 17:00:46 +0700

    Codelab 8

---
+++ Codelabs7/js/JavaScript.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $(".nav-bar-item").mouseenter(function () {
+        $(".sub-nav").hide();
+        $(this).children().show()
+    });
+    $(".nav-bar-item").click(function () {
+        $(".sub-nav").hide();
+    });
+    $(".nav-bar-item").mouseleave(function () {
+        $(".sub-nav").hide();
+    })
+})
