499e9cb0461c6b2ba36163e72fb37704bd026bb1
Zach00 <hieptrong790@gmail.com>
2020-08-24 16:09:15 +0700

    Home-Responsive

--- codelabs5/Trong Hiep/js/dist/home.dev.js
+++ codelabs5/Trong Hiep/js/dist/home.dev.js
@@ -1,12 +1,11 @@
-$(document).ready(function(){
-  $("#mens").click(function(){
+"use strict";
+
+$(document).ready(function () {
+  $("#mens").click(function () {
     $("#sub-menu-1").toggle();
   });
-});
-
-// var mens = document.getElementById("mens");
+}); // var mens = document.getElementById("mens");
 // var subMenu1 = document.getElementById("sub-menu-1");
-
 // subMenu1.style.display = "none";
 // mens.onclick = function(){
 //   if(subMenu1.style.display = "none"){
